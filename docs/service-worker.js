2aee369dd84d97a820e3946c8a588e4f0f0f80ea
Oleg Smyk <Smyk006@yandex.ru>
2018-11-06 23:27:58 +0300

    upload production

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2fdf62c45295c1c4b17c62a0e26a245.js"
+  "/onpoint_test/precache-manifest.438fe435dd6f6a077a06114c2553fa23.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/onpoint_test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
