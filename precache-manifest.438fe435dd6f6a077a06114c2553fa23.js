49da62f95c635243082fee75d27390c6f5924ec3
Oleg Smyk <Smyk006@yandex.ru>
2018-11-06 23:27:28 +0300

    Updates

---
+++ precache-manifest.438fe435dd6f6a077a06114c2553fa23.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b15c3d4bb7a946d1d2f90c1300c690d",
+    "url": "/onpoint_test/static/media/ContentHeaderBackground.1b15c3d4.png"
+  },
+  {
+    "revision": "e9dfd0cf212cee69c1b3",
+    "url": "/onpoint_test/static/css/main.23d334b4.chunk.css"
+  },
+  {
+    "revision": "eefe67e3900f8a8a292d2c5d78833ebf",
+    "url": "/onpoint_test/static/media/ice_2.eefe67e3.png"
+  },
+  {
+    "revision": "d01194eb3888f1266b4a",
+    "url": "/onpoint_test/static/js/1.d01194eb.chunk.js"
+  },
+  {
+    "revision": "ce4219fbbc4b6d2ab9c3",
+    "url": "/onpoint_test/static/js/runtime~main.ce4219fb.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/onpoint_test/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/onpoint_test/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/onpoint_test/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/onpoint_test/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b8c54ceaf324394784825d97878b39cb",
+    "url": "/onpoint_test/static/media/next.b8c54cea.png"
+  },
+  {
+    "revision": "e9dfd0cf212cee69c1b3",
+    "url": "/onpoint_test/static/js/main.e9dfd0cf.chunk.js"
+  },
+  {
+    "revision": "062b47d3d1c6d57928eb335a4625968f",
+    "url": "/onpoint_test/static/media/ContentCentralBackground.062b47d3.png"
+  },
+  {
+    "revision": "0e82242de90a943617a4e37eae7eba14",
+    "url": "/onpoint_test/static/media/ContentFooterBackground.0e82242d.png"
+  },
+  {
+    "revision": "8b6cc85a49a2acca9e73e45632ba6f3e",
+    "url": "/onpoint_test/static/media/ice_3.8b6cc85a.png"
+  },
+  {
+    "revision": "4d68bb268393907f17690d2dbd507f48",
+    "url": "/onpoint_test/static/media/tab_2.4d68bb26.png"
+  },
+  {
+    "revision": "6d647ed6a463d125f7720a3b67e93b86",
+    "url": "/onpoint_test/static/media/tab_3.6d647ed6.png"
+  },
+  {
+    "revision": "d74d507fb2b319d8a2fdfc28bbc56e45",
+    "url": "/onpoint_test/static/media/ice_1.d74d507f.png"
+  },
+  {
+    "revision": "9da963824e777c15a6be2c64f35e5210",
+    "url": "/onpoint_test/static/media/tab_1.9da96382.png"
+  },
+  {
+    "revision": "d01194eb3888f1266b4a",
+    "url": "/onpoint_test/static/css/1.8c73e2d5.chunk.css"
+  },
+  {
+    "revision": "2beebcafa207820e1f0a92fde4836161",
+    "url": "/onpoint_test/index.html"
+  }
+];
